4b15a048ee746124bc245acc0554480eab9e7dd2
Ho.Chun <hochun836@gmail.com>
2020-12-08 14:38:13 +0800

    test change detection

--- src/app/highcharts/highcharts.component.ts
+++ src/app/highcharts/highcharts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { option1 } from './data';
 
@@ -6,10 +6,12 @@ declare var require: any;
 let Boost = require('highcharts/modules/boost');
 let noData = require('highcharts/modules/no-data-to-display');
 let More = require('highcharts/highcharts-more');
+let Export = require('highcharts/modules/exporting');
 
 Boost(Highcharts);
 noData(Highcharts);
 More(Highcharts);
+Export(Highcharts);
 
 @Component({
   selector: 'app-highcharts',
@@ -18,9 +20,13 @@ More(Highcharts);
 })
 export class HighchartsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private zone: NgZone,
+  ) { }
 
   ngOnInit(){
+    this.zone.onUnstable.subscribe(() => { console.log('# 事件發生了') });
+    this.zone.onStable.subscribe(() => { console.log('@ 事件結束了') });
     Highcharts.chart('container1', option1);
   }
 }
